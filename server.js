f706efe4a4ed95039dd9d6b18e4d1a1603052f23
Junhan Huang <robinhuang@Junhans-MacBook-Pro.local>
2017-01-22 17:21:09 -0500

    added resave option on express-session

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session({
     secret: 'knowledgeIsPower', // session secret
-    saveUninitialized: true
+    saveUninitialized: true,
+    resave: true
 }));
 app.use(passport.initialize());
 app.use(passport.session());
