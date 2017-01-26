f27631249908d0a51204bccce252311464fece4f
Junhan Huang <robinhuang@Junhans-MacBook-Pro.local>
2017-01-25 22:00:00 -0500

    creating a Book object out of search results now

--- src/app/book-search/book-search.component.ts
+++ src/app/book-search/book-search.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GoodReadsService } from '../goodreads.service';
-
+import { Book } from './book/book';
 @Component({
   selector: 'book-search',
   templateUrl: './book-search.component.html',
@@ -8,15 +8,23 @@ import { GoodReadsService } from '../goodreads.service';
 })
 export class BookSearchComponent {
   searchResult: any;
-  private searchResultString: String;
-  searchQuery: String;
+  searchQuery: string;
   errorMessage: any;
 
   // Field Data
-  private srTitle: String;
-  private srAuthor: String;
-  private srDescription: String;
-  private srimageURL: String;
+  private srTitle: string;
+  private srAuthor: string;
+  private srDescription: string;
+  private srimageURL: string;
+
+  private isbn: string;
+  private isbn13: string;
+  private avg_rating: number;
+  private num_pages: number;
+  private publisher: string;
+  private gr_url: string;
+  
+  private resultBook: Book;
 
   constructor(private goodReadsService: GoodReadsService) {
     this.srimageURL = "";
@@ -27,7 +35,7 @@ export class BookSearchComponent {
       .subscribe(
         searchResult => {
           this.searchResult = searchResult;
-          console.log(searchResult);
+          //console.log(searchResult);
           this.extractData();
           },
         error => this.errorMessage = <any> error
@@ -40,7 +48,15 @@ export class BookSearchComponent {
     this.srTitle = this.searchResult.book[0].title[0];
     this.srAuthor = this.searchResult.book[0].authors[0].author[0].name[0];
     this.srDescription = this.searchResult.book[0].description[0];
+
+    this.isbn = this.searchResult.book[0].isbn[0];
+    this.isbn13 = this.searchResult.book[0].isbn13[0];
+    this.avg_rating = this.searchResult.book[0].average_rating[0];
+    this.num_pages = Number.parseInt(this.searchResult.book[0].num_pages[0]);
+    this.publisher = this.searchResult.book[0].publisher[0];
+    this.gr_url = this.searchResult.book[0].url[0];
     
+    this.resultBook = new Book (this.srTitle, this.srAuthor, this.isbn, this.isbn13, this.srDescription, this.srimageURL, this.avg_rating, this.num_pages, this.publisher, this.gr_url);
   }
 
 }
